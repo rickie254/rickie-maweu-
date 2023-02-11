6e052a153760914c7747acd88e6cee84cf4aac51
rickie maweu <rickie.maweu@student.moringaschool.com>
2023-02-11 11:22:06 +0300

    changes

--- src/sections/header/data.js
+++ src/sections/header/data.js
@@ -7,8 +7,8 @@ import {BsMedium} from 'react-icons/bs'
 
 
 const data = [
-    {id: 1, link: 'https://www.instagram.com/boyani_sylvia/', icon: <AiOutlineInstagram/>},
-    {id: 1, link: 'https://twitter.com/BoyaniSylvia', icon: <AiOutlineTwitter/>},
+    {id: 1, link: 'https://www.instagram.com/issa.ranxxs/', icon: <AiOutlineInstagram/>},
+    {id: 1, link: 'https://twitter.com/issaranxxs254', icon: <AiOutlineTwitter/>},
     {id: 1, link: 'https://github.com/sylvia-boyani', icon: <AiFillGithub/>},
     {id: 1, link: 'https://www.linkedin.com/in/sylvia-boyani-7130a6196/', icon: <AiFillLinkedin/>},
     {id: 1, link: 'https://web.facebook.com/profile.php?id=100089759584527', icon: <AiFillFacebook/>},
