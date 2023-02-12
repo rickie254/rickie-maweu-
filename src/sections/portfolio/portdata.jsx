47ad379f57baa5999d30df6e046107e1d53b317c
rickie maweu <rickie.maweu@student.moringaschool.com>
2023-02-12 11:20:39 +0300

    changes

--- src/sections/portfolio/portdata.jsx
+++ src/sections/portfolio/portdata.jsx
@@ -1,28 +1,29 @@
 const Menu = [
     {
         id: 1,
-        image:"https://raw.githubusercontent.com/sylvia-boyani/signing-system/main/assets/images/home-page.png",
+        
+        image:"https://i.pinimg.com/564x/c5/34/cf/c534cf82216a85f1520ced54b9dea272.jpg",
         heading:"Signing-in App",
         paragrapgh:"Website",
         category:"frontend"
     },
     {
         id: 2,
-        image:"https://raw.githubusercontent.com/sylvia-boyani/do-my-hair/main/public/home.png",
-        heading:"Hair on Flick",
+        image:"https://i.pinimg.com/564x/89/23/dc/8923dc82e2474a97a135ebf5ea07e5c3.jpg",
+        heading:"movie apk",
         paragrapgh:"Website",
         category:"frontend"
     },
     {
         id: 3,
-        image:"https://raw.githubusercontent.com/sylvia-boyani/phase2-project/main/public/images/Screenshot%20from%202022-07-31%2003-25-03.png",
+        image:"https://i.pinimg.com/564x/67/fc/b3/67fcb3f569ace159636c838e11ffece4.jpg",
         heading:"Travel",
         paragrapgh:"Adventure",
         category:"Designing"
     },
     {
         id: 4,
-        image:"https://raw.githubusercontent.com/sylvia-boyani/phase2-project/main/public/images/Screenshot%20from%202022-07-31%2003-25-20.png",
+        image:"https://i.pinimg.com/564x/bc/c7/04/bcc70456e740eaae1a9d9fd8041b6086.jpg",
         heading:"Documentary",
         paragrapgh:"Discover",
         category:"Backend"
@@ -30,17 +31,17 @@ const Menu = [
     },
     {
         id: 5,
-        image:"https://raw.githubusercontent.com/sylvia-boyani/hope/master/src/assets/tumaini.png",
-        heading:"Website",
-        paragrapgh:"Design",
+        image:"https://i.pinimg.com/564x/06/84/4c/06844c1bdc0c0cb5c8a42e5c1fa4304e.jpg",
+        heading:"tv web apk",
+        paragrapgh:"website",
         category:"Designing"
     },
     {
         id: 6,
-        image:"https://raw.githubusercontent.com/sylvia-boyani/me/main/src/assets/ME.png",
-        heading:"Portfolio",
-        paragrapgh:"Optimiazation",
-        category:"Portfolio"
+        image:"https://i.pinimg.com/564x/76/5f/59/765f59075f3ba1e9623d2c9e9d6ca67e.jpg",
+        heading:"E-comm web apk",
+        paragrapgh:"website",
+        category:"E-comm web apk"
     },
 ]
 export default Menu
